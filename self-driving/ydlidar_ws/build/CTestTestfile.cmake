af06606d13d00858297dcd1a53db6fe1cbdca7c7
MJ <kmj99759@naver.com>
2020-02-19 03:01:43 +0900

    input ydlidar package

---
+++ self-driving/ydlidar_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/pirl/ydlidar_ws/src
+# Build directory: /home/pirl/ydlidar_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("x2_cartographer/carto_mapper")
+subdirs("ydlidar")
